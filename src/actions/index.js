150d4efc024c3edbb356f383d67ab89177b9d751
Gabriel Diez <gabriel.diez@rosariosoftware.com>
2020-03-05 21:30:48 -0300

    Add onesignal

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,7 @@ import {
     getCountries,
     setReward
 } from '../api'
+import OneSignal from 'react-native-onesignal';
 
 export const selectedInterests = (interest) => {
     return {
@@ -155,10 +156,14 @@ export const login = (token) => {
 */
 export const register = (user_IG) => {
     return async (dispatch) => {
+        OneSignal.init("05fc4295-e955-49d7-adc0-5921cc1357de", {kOSSettingsKeyAutoPrompt : true});
         dispatch(getData())
-        const user_ST = await registerUser(user_IG)
-        dispatch(userSetData(user_ST))
-        dispatch(getDataSuccess([]))
+        OneSignal.addEventListener('ids', async function(device) {
+            user_IG.player_id = device.userId
+            const user_ST = await registerUser(user_IG)
+            dispatch(userSetData(user_ST))
+            dispatch(getDataSuccess([]))
+        });
     }
 }
 
