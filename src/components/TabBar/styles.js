dcea518f26d1eb2a0af2465fe7ecaa21d3336db7
Pablo14697 <pablomed88@gmail.com>
2020-04-12 00:25:23 -0300

    Pm tab navigator and profile (#26)
    * Tab navigator
    
    * Tab navigator and match,tag,interest icons in profile
    
    * Tab navigator
    
    Co-authored-by: Pablo Emanuel Medina <pablo@Pablos-Mac-mini.local>

---
+++ src/components/TabBar/styles.js
@@ -0,0 +1,16 @@
+// LIBS
+import {StyleSheet} from 'react-native';
+// STYLES
+
+// ────────────────────────────────────────────────────────────────────────
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    height: 60,
+    justifyContent: 'space-around',
+  },
+});
+
+export default styles;
--- src/navigation/tabNavigation.js
+++ src/navigation/tabNavigation.js
@@ -5,6 +5,7 @@ import {Icon} from 'native-base';
 import SelectInterests from '../views/selectInterests/selectInterests';
 import CompatibleProfileStack from './compatibleProfilesNavigator';
 import ProfileStack from './profileNavigation';
+import TabBar from '../components/TabBar';
 
 /*
     En este .js se configura el TabBar que aparece en la parte de abajo
@@ -31,52 +32,11 @@ const TabNavigator = createMaterialTopTabNavigator(
     },
   },
   {
+    tabBarComponent: props => TabBar(props),
+
     initialRouteName: 'SelectInterests',
-    defaultNavigationOptions: ({navigation}) => ({
-      tabBarIcon: ({focused, horizontal, tintColor}) => {
-        const {routeName} = navigation.state;
-        let iconName;
-        if (routeName === 'CompatibleProfileStack') {
-          iconName = 'heart';
-        } else if (routeName === 'Profile') {
-          iconName = 'user';
-        } else if (routeName === 'SelectInterests') {
-          iconName = 'thumbs-up';
-        }
-        return (
-          <Icon type="FontAwesome" name={iconName} style={{color: tintColor}} />
-        );
-      },
-    }),
-    tabBarOptions: {
-      activeTintColor: '#EE4B3B',
-      inactiveTintColor: 'grey',
-      showIcon: true,
-      style: {
-        backgroundColor: 'rgba(0, 0, 0, 1)',
-        elevation: 8,
-        padding: 0,
-        margin: 0,
-        paddingTop: 5,
-      },
-      indicatorStyle: {
-        height: 0,
-      },
-      labelStyle: {
-        fontSize: 10,
-        margin: 0,
-        padding: 0,
-        paddingTop: 10,
-      },
-      iconStyle: {
-        width: 30,
-        height: 30,
-        padding: 0,
-        margin: 0,
-      },
-    },
+
     tabBarPosition: 'bottom',
-    backBehavior: 'history',
   },
 );
 
