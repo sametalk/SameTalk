dde076f694326cbb81dd7fa8045c4c738bf48136
Pablo14697 <pablomed88@gmail.com>
2020-04-13 02:36:15 -0300

    Pm first screen (#34)
    * Settings screen is ready
    
    * Interest lists added
    
    * Work in register screen
    
    * register screen is ready
    
    * select country is ready
    
    * gene screen is ready
    
    Co-authored-by: Pablo Emanuel Medina <pablo@Pablos-Mac-mini.local>

--- src/navigation/appNavigator.js
+++ src/navigation/appNavigator.js
@@ -1,60 +1,63 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import Register from '../views/registerViews/register'
-import Welcome from '../views/welcome'
-import SelectCountry from '../views/registerViews/selectCountry'
-import SelectAge from '../views/registerViews/selectAge' 
-import SelectSex from '../views/registerViews/selectSex'
-import TabNavigation from './tabNavigation'
-import Prueba from '../views/prueba'
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Register from '../views/registerViews/register';
+import Welcome from '../views/welcome';
+import SelectCountry from '../views/registerViews/selectCountry';
+import SelectAge from '../views/registerViews/selectAge';
+import SelectSex from '../views/registerViews/selectSex';
+import TabNavigation from './tabNavigation';
+import Prueba from '../views/prueba';
 
 /*
   Navegacion desde la pantalla de Welcome hasta la pantalla del TabBarBottom (TabNavigation)
   Pasa por todo el proceso de registro
 */
-const AppNavigator = createStackNavigator({
-  Welcome: {
-    screen: Welcome,
-    navigationOptions: () => ({
-      header: null
-    }),
+const AppNavigator = createStackNavigator(
+  {
+    Welcome: {
+      screen: Welcome,
+      navigationOptions: () => ({
+        header: null,
+      }),
+    },
+    Register: {
+      screen: Register,
+      navigationOptions: () => ({
+        header: null,
+      }),
+    },
+    SelectAge: {
+      screen: SelectAge,
+      navigationOptions: () => ({
+        header: null,
+      }),
+    },
+    SelectCountry: {
+      screen: SelectCountry,
+      navigationOptions: () => ({
+        header: null,
+      }),
+    },
+    SelectSex: {
+      screen: SelectSex,
+      navigationOptions: () => ({
+        header: null,
+      }),
+    },
+    TabNavigation: {
+      screen: TabNavigation,
+      navigationOptions: () => ({
+        header: null,
+      }),
+    },
+    Prueba: {
+      screen: Prueba,
+      navigationOptions: () => ({
+        header: null,
+      }),
+    },
   },
-  Register: {
-    screen: Register,
-    navigationOptions: () => ({
-      header: null
-    }),
-  },
-  SelectAge:{
-    screen: SelectAge,
-    navigationOptions: () => ({
-      title: `Registro:`
-    }),
-  },
-  SelectCountry: {
-    screen: SelectCountry,
-    navigationOptions: () => ({
-      title: `Registro:`
-    }),
-  },
-  SelectSex: {
-    screen: SelectSex,
-    navigationOptions: () => ({
-      title: `Registro:`
-    }),
-  },
-  TabNavigation: {
-    screen: TabNavigation,
-    navigationOptions: () => ({
-      header: null
-    })
-  },
-  Prueba: {
-    screen: Prueba,
-    navigationOptions: () => ({
-      header: null
-    })
-  }
-}, { initialRouteName: 'Welcome'});
+  {initialRouteName: 'Welcome'},
+);
 
 export default createAppContainer(AppNavigator);
